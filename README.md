faf9c33ca28f4aa636e2ad866fa9979c5c794780
Laura Semesky <lsemesky@gmail.com>
2019-07-14 16:03:58 -0400

    updating documentation

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ You will also see any lint errors in the console.
 ### `npm test`
 
 Launches the test runner in the interactive watch mode.<br>
+To run all tests, make sure to press `a` when prompted.
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm run storybook`
--- src/components/Particle/Particle.test.js
+++ src/components/Particle/Particle.test.js
@@ -1,27 +1,29 @@
 import Particle from './Particle'
 import React from 'react'
+import { leafImageArray } from '../../svg/leaves'
 import { shallow } from 'enzyme'
 
+const image = leafImageArray[0]
 it('renders without crashing', () => {
-  shallow(<Particle />)
+  shallow(<Particle image={image} />)
 })
 
 it('changes the svg style based on the height property', () => {
   let customHeight = 23
-  let wrapper = shallow(<Particle height={customHeight} />)
-  expect(wrapper.props().style.height).toBe(customHeight + 'px')
+  let wrapper = shallow(<Particle height={customHeight} image={image} />)
+  expect(wrapper.props().style.height).toBe(customHeight)
 
   customHeight = 99
-  wrapper = shallow(<Particle height={customHeight} />)
-  expect(wrapper.props().style.height).toBe(customHeight + 'px')
+  wrapper = shallow(<Particle height={customHeight} image={image} />)
+  expect(wrapper.props().style.height).toBe(customHeight)
 })
 
 it('changes the svg fill style based on the color property', () => {
   let customColor = 'RebeccaPurple'
-  let wrapper = shallow(<Particle color={customColor} />)
+  let wrapper = shallow(<Particle color={customColor} image={image} />)
   expect(wrapper.props().style.fill).toBe(customColor)
 
   customColor = '#123456'
-  wrapper = shallow(<Particle color={customColor} />)
+  wrapper = shallow(<Particle color={customColor} image={image} />)
   expect(wrapper.props().style.fill).toBe(customColor)
 })
